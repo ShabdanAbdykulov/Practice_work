9abf86f2070a05841f18034a2f8cc23b724401a3
Shabdan <abdykulovshabdan@gmail.com>
2022-12-16 18:35:30 +0600

    Практическое задание 4

---
+++ practice02/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 7;
+int b = 2;
+int c = 3;
+
+int max = a;
+
+if (a > max) max =a;
+if (b > max) max =b;
+if (c > max) max =c;
+
+System.Console.WriteLine(max);
+
+int d = 44;
+int e = 5;
+int i = 78;
+
+if (d > max) max =d;
+if (e > max) max =e;
+if (i > max) max =i;
+
+System.Console.WriteLine(max);
+
