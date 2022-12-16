e36add50dbf34c70e75dc123aaca569ad6afc106
Shabdan <abdykulovshabdan@gmail.com>
2022-12-16 18:51:17 +0600

    Правки

--- practice02/Program.cs
+++ practice02/Program.cs
@@ -4,19 +4,17 @@ int c = 3;
 
 int max = a;
 
-if (a > max) max =a;
 if (b > max) max =b;
 if (c > max) max =c;
 
 System.Console.WriteLine(max);
 
-int d = 44;
-int e = 5;
-int i = 78;
+a = 44;
+b = 5;
+c = 78;
 
-if (d > max) max =d;
-if (e > max) max =e;
-if (i > max) max =i;
+if (b > max) max =b;
+if (c > max) max =c;
 
 System.Console.WriteLine(max);
 
