7ab14947df3f94dd55113a6c670b2dceb51f594a
Shabdan <abdykulovshabdan@gmail.com>
2022-12-17 15:49:39 +0600

    Практическое задание 6

---
+++ practice03/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int number = 4;
+int number2 = -3;
+int number3 = 7;
+
+System.Console.WriteLine(number % 2 == 0);
+System.Console.WriteLine(number2 % 2 == 0);
+System.Console.WriteLine(number3 % 2 == 0);
